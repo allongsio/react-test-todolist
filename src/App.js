be37927fb5920f09abed923bf13740971d17dffa
allongsio <choisio2301@gmail.com>
2023-04-20 10:52:31 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [title, setTitle] = useState("");
+  const [list, setList] = useState([]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      <InputField
+        title={title}
+        setTitle={setTitle}
+        list={list}
+        setList={setList}
+      ></InputField>
+      <div>
+        <TodoCardList list={list} setList={setList}></TodoCardList>
+      </div>
+    </div>
+  );
+}
+
+function TodoCardList({ list, setList }) {
+  return (
+    <div className="todo-card-list">
+      {list.map((item) => {
+        return <div className="item-card">{item}</div>;
+      })}
+    </div>
+  );
+}
+
+function InputField({ title, setTitle, list, setList }) {
+  const onChangeHandler = (event) => {
+    setTitle(event.target.value);
+  };
+
+  const onSubmitHandler = () => {
+    let newTodoList = [...list];
+    !newTodoList.includes(title)
+      ? newTodoList.push(title)
+      : (newTodoList = newTodoList);
+    setList(newTodoList);
+    setTitle("");
+  };
+  return (
+    <div className="title-field">
+      <div className="input-field">
+        <input type="text" value={title} onChange={onChangeHandler}></input>
+        <button onClick={onSubmitHandler}>추가하기</button>
+      </div>
+      <div className="title-text">
+        <h3>Todo List</h3>
+      </div>
     </div>
   );
 }
